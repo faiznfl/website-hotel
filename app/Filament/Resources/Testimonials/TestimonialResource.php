0371ae93344c53671665ba29210a3ace7e37eca9
faiznfl <faiznfl20@gmail.com>
2026-01-22 13:55:20 +0700

    update icon review

--- app/Filament/Resources/Testimonials/TestimonialResource.php
+++ app/Filament/Resources/Testimonials/TestimonialResource.php
@@ -29,7 +29,9 @@ class TestimonialResource extends Resource
 {
     protected static ?string $model = Testimonial::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-chat-bubble-bottom-center-text';
+
+    protected static ?string $navigationLabel = "Review";
 
     public static function form(Schema $schema): Schema
     {
